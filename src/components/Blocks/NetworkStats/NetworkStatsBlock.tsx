3f9d3f4a03f8602c9f1d9cebc429c3a114c61c8c
vladimir <vlaoff@gmail.com>
2022-07-28 10:48:46 +0300

    network stats

--- src/components/Blocks/NetworkStats/NetworkStatsBlock.tsx
+++ src/components/Blocks/NetworkStats/NetworkStatsBlock.tsx
@@ -1,6 +1,8 @@
 import { NetworkStat, Sale } from './types'
 import NetworkStatItem from './NetworkStatItem'
 import NetworkStatsSale from './NetworkStatsSale'
+import styles from './NetworkStats.module.scss'
+import classnames from 'classnames'
 
 const stats: NetworkStat[] = [
   {
@@ -82,12 +84,25 @@ const NetworkStatsBlock = () => {
       <div className="g-container">
         <h2 className="t-block-title t-dark">Network Stats</h2>
         <div className="ml-10">
-          <div className="grid">
+          <div className={classnames(styles.StatsGrid, 'grid gap-6 mb-16 max-w-[1340px]')}>
             {stats.map((x, index) => (
               <NetworkStatItem {...x} key={index} />
             ))}
           </div>
 
+          <h3 className="font-heading text-24 t-dark font-bold mb-11">Past Sales</h3>
+          <div
+            className={classnames(styles.StatSale, 'px-6 h-[66px] items-center text-12 font-bold')}>
+            <div>Project Name</div>
+            <div>IDO Token Price</div>
+            <div>Current Price</div>
+            <div>ATH</div>
+            <div>No. Registrations</div>
+            <div>Total Raised</div>
+            <div>Total Tokens Sold</div>
+            <div>Sale Ended At</div>
+            <div className="text-right">ROI</div>
+          </div>
           {sales.map((x, index) => (
             <NetworkStatsSale {...x} key={index} />
           ))}
--- src/components/Blocks/NetworkStats/NetworkStatsSale.tsx
+++ src/components/Blocks/NetworkStats/NetworkStatsSale.tsx
@@ -1,12 +1,27 @@
 import { Sale } from './types'
 import React from 'react'
+import styles from './NetworkStats.module.scss'
+import classNames from 'classnames'
+import Logo from 'assets/images/zk-lend.svg'
 
 const NetworkStatsSale: React.FC<Sale> = (sale) => {
+  console.log({
+    Logo,
+  })
   return (
-    <div className="NetworkStatsSale">
-      <div className="icon"></div>
-      <div className="name">
-        {sale.name} {sale.tokenName}
+    <div
+      className={classNames(
+        styles.StatSale,
+        'NetworkStatsSale font-heading font-bold text-12 block grid px-6 py-4 mb-4 ui-t-primary items-center'
+      )}>
+      <div className="name flex">
+        <div className="icon mr-5">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="text">
+          {sale.name} <br />
+          <span className="font-sans"> {sale.tokenName}</span>
+        </div>
       </div>
       <div className="token-price">{sale.idoPrice}</div>
       <div>{sale.currentPrice}</div>
@@ -15,7 +30,10 @@ const NetworkStatsSale: React.FC<Sale> = (sale) => {
       <div>{sale.totalRaised}</div>
       <div>{sale.totalTokens}</div>
       <div>{sale.saleEnded}</div>
-      <div>{Math.round(sale.currentPrice / sale.idoPrice)}</div>
+      <div className="text-right text-24">
+        {Math.round(sale.currentPrice / sale.idoPrice)}
+        <span className="text-20">x</span>
+      </div>
     </div>
   )
 }
