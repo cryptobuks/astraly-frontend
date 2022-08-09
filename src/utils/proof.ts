0330f7ffbca6707fac001536b2daabe8c6f23e5f
Filip Laurentiu <f.laurentiu26@gmail.com>
2022-08-09 10:27:06 +0300

    Add missing utils and packages

--- src/utils/proof.ts
+++ src/utils/proof.ts
@@ -1,5 +1,44 @@
 import { sha3Raw, stripHexPrefix, toHex, padLeft, toBN } from 'web3-utils'
 import { utils } from 'ethers'
+import { secp256k1 } from '@zoltu/ethereum-crypto'
+import * as BN from 'bn.js'
+
+function packInts64(input: string): Array<BN> {
+  const array = []
+  for (const s of input.match(/.{1,16}/g)!) {
+    array.push(toBN(s))
+  }
+  return array
+}
+
+function packBigInt3(input: string): Array<BN> {
+  const a = []
+  const base = toBN(2).pow(toBN(86))
+  let num = toBN(input)
+  for (const _ of [0, 1, 2]) {
+    // how to create for loop with unused variable?
+    const quotient = num.div(base)
+    const residue = num.mod(base)
+    num = quotient
+    a.push(residue)
+  }
+  return a
+}
+
+function encodeProof(proof: Array<string>) {
+  const flatProof: Array<BN> = []
+  const flatProofSizesBytes = []
+  const flatProofSizesWords = []
+
+  for (const proofElement of proof) {
+    const packedProofElement: Array<BN> = packInts64(proofElement)
+    flatProof.push(...packedProofElement)
+    flatProofSizesBytes.push(stripHexPrefix(proofElement).length / 2)
+    flatProofSizesWords.push(packedProofElement.length)
+  }
+
+  return [flatProof, flatProofSizesWords, flatProofSizesBytes]
+}
 
 export async function encodeCallArgs(
   provider: any,
@@ -9,55 +48,50 @@ export async function encodeCallArgs(
   token: string,
   blockNumber: number,
   storageSlot: string,
-  balance: string,
+  balance: string
 ) {
-  const number = "0x"+blockNumber.toString(16)
-  const block = await provider?.send("eth_getBlockByNumber", [number, false]);
+  const number = '0x' + blockNumber.toString(16)
+  const block = await provider?.send('eth_getBlockByNumber', [number, false])
 
-  console.log(block);
+  console.log(block)
 
   // Prepare message attestation contents
   let pos = padLeft(stripHexPrefix(ethAccount.toLowerCase()), 64)
   pos += padLeft(stripHexPrefix(toHex(storageSlot)), 64)
-  const storageKey = sha3Raw("0x"+pos)
-  const starknetAccount = stripHexPrefix(starknetAccount_);
-  const stateRoot = stripHexPrefix(block.stateRoot);
+  const storageKey = sha3Raw('0x' + pos)
+  const starknetAccount = stripHexPrefix(starknetAccount_)
+  const stateRoot = stripHexPrefix(block.stateRoot)
 
   // Sign attestation message
-  const message = starknetAccount + stateRoot + stripHexPrefix(storageKey);
-  const paddedMessage = "000000" + message + "00000000";
-  let packedMessage : Array<BN> = [
-    toBN("1820989616068650357"),
-    toBN("7863376661560845668"),
-    toBN("2327628128951822181"),
-    toBN("4182209287050756096")];
-  packedMessage.push(...packInts64(paddedMessage));
-  const rawSignature = await provider?.getSigner().signMessage(paddedMessage);
-  const signature = utils.splitSignature(rawSignature);
+  const message = starknetAccount + stateRoot + stripHexPrefix(storageKey)
+  const paddedMessage = '000000' + message + '00000000'
+  const packedMessage: Array<BN> = [
+    toBN('1820989616068650357'),
+    toBN('7863376661560845668'),
+    toBN('2327628128951822181'),
+    toBN('4182209287050756096'),
+  ]
+  packedMessage.push(...packInts64(paddedMessage))
+  const rawSignature = await provider?.getSigner().signMessage(paddedMessage)
+  const signature = utils.splitSignature(rawSignature)
 
   // Derive the y-coordinate of the elliptic curve point
-  const Rx = BigInt("0x"+toBN(signature.r).toJSON());
-  const recoveryParam : 0|1 = signature.recoveryParam === 0 ? 0 : 1;
-  const Ry = secp256k1.decompressPoint(Rx, recoveryParam);
-  console.log("Rx", Rx);
-  console.log("Ry", Ry);
+  const Rx = BigInt('0x' + toBN(signature.r).toJSON())
+  const recoveryParam: 0 | 1 = signature.recoveryParam === 0 ? 0 : 1
+  const Ry = secp256k1.decompressPoint(Rx, recoveryParam)
+  console.log('Rx', Rx)
+  console.log('Ry', Ry)
 
   // Request storage state proof
-  const proof = await provider?.send("eth_getProof", [
-      token,
-      [storageKey],
-      number]);
-  const accountProof = proof.accountProof;
-  const storageProof = proof.storageProof[0];
-  const [
-    accountProofsConcat,
-    accountProofSizesWords,
-    accountProofSizesBytes] = encodeProof(accountProof);
-  const [
-    storageProofsConcat,
-    storageProofSizesWords,
-    storageProofSizesBytes] = encodeProof(storageProof.proof);
-  console.log(proof);
+  const proof = await provider?.send('eth_getProof', [token, [storageKey], number])
+  const accountProof = proof.accountProof
+  const storageProof = proof.storageProof[0]
+  const [accountProofsConcat, accountProofSizesWords, accountProofSizesBytes] =
+    encodeProof(accountProof)
+  const [storageProofsConcat, storageProofSizesWords, storageProofSizesBytes] = encodeProof(
+    storageProof.proof
+  )
+  console.log(proof)
 
   console.log(toBN(starknetAccount_))
   console.log(toBN(balance))
@@ -117,5 +151,5 @@ export async function encodeCallArgs(
     storageProofsConcat,
     storageProofSizesWords,
     storageProofSizesBytes,
-  ];
+  ]
 }
